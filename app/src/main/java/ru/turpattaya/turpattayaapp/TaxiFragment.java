46154c0c244246477897c9f9ea7ed5a829d448e0
Alshin Anton <AlshinAnton@gmail.com>
2016-11-09 20:28:42 +0700

    Add 2 spinners thx Masha

--- app/src/main/java/ru/turpattaya/turpattayaapp/TaxiFragment.java
+++ app/src/main/java/ru/turpattaya/turpattayaapp/TaxiFragment.java
@@ -15,18 +15,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class TaxiFragment extends Fragment {
     private Spinner spinnerFrom;
     private Spinner spinnerTo;
     private Spinner spinnerCar;
+
     private TextView priceRezult;
     private Button buttonOrderTaxi;
 
-/*    private ArrayList listTaxiFrom;
-    private ArrayList listTaxiDestination;
-    private ArrayList listCarType;*/
+    HashMap<String, String> fromPares;
+    HashMap<String, String> destinationPares;
+    ArrayList<String> destinationArray;
+
+
     MySQLiteHelper helper;
 
 
@@ -47,7 +51,9 @@ public class TaxiFragment extends Fragment {
         spinnerTo.setPrompt("Куда");
         spinnerCar.setPrompt("Тип машины");
 
-        MySQLiteHelper helper = new MySQLiteHelper(getActivity());
+        helper = new MySQLiteHelper(getActivity());
+
+
 
         Cursor cursor = helper.getReadableDatabase().query(
                 TaxiFromTable.TABLE_TAXIFROMTABLE,
@@ -62,9 +68,18 @@ public class TaxiFragment extends Fragment {
 
         ArrayList<String> fromArray = new ArrayList<>();
 
+        fromPares = new HashMap<>();
+
+        fromArray.add("Выберите откуда");
+
         if (cursor.moveToFirst()) {
             while (!cursor.isAfterLast()) {
-                fromArray.add(cursor.getString(cursor.getColumnIndexOrThrow(TaxiFromTable.COLUMN_TAXIFROM_FROMRUSSIANNAME)));
+                String name = cursor.getString(cursor.getColumnIndexOrThrow(TaxiFromTable.COLUMN_TAXIFROM_FROMRUSSIANNAME));
+                String code = cursor.getString(cursor.getColumnIndexOrThrow(TaxiFromTable.COLUMN_TAXIFROM_FROMCODE));
+
+                fromArray.add(name);
+                fromPares.put(name, code);
+
                 cursor.moveToNext();
             }
             cursor.close();
@@ -74,38 +89,26 @@ public class TaxiFragment extends Fragment {
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerFrom.setAdapter(spinnerArrayAdapter);
 
-        Cursor cursorDestination = helper.getReadableDatabase().query(
-                TaxiDestinationTable.TABLE_TAXIDESTINATIONTABLE,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null
-        );
 
-        ArrayList<String> destinationArray = new ArrayList<>();
 
-        if (cursor.moveToFirst()) {
-            while (!cursor.isAfterLast()) {
-                destinationArray.add(cursor.getString(cursor.getColumnIndexOrThrow(TaxiDestinationTable.COLUMN_TAXIDESTINATION_DESTINATIONRUSSIANNAME)));
-                cursor.moveToNext();
-            }
-            cursor.close();
-        }
 
-        spinnerArrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, destinationArray); //selected item will look like a spinner set from XML
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerTo.setAdapter(spinnerArrayAdapter);
+
 
 
         spinnerFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(parent.getContext(),
-                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
-                        Toast.LENGTH_SHORT).show();
+
+                String selectedText = parent.getItemAtPosition(position).toString();
+                if (selectedText.contains("Выберите")) {
+                    spinnerTo.setClickable(false);
+                    return;
+                }
+                if (fromPares.containsKey(selectedText)) {
+                    String selectedCode = fromPares.get(selectedText);
+                    fillDestinationSpinner(selectedCode);
+                    spinnerTo.setClickable(true);
+                }
             }
 
             @Override
@@ -117,9 +120,7 @@ public class TaxiFragment extends Fragment {
         spinnerTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(parent.getContext(),
-                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
-                        Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -149,6 +150,48 @@ public class TaxiFragment extends Fragment {
         // TODO Auto-generated method stub
     }
 
+    private void fillDestinationSpinner(String fromCode) {
+
+        destinationArray = new ArrayList<>();
+        destinationArray.add("Выберите куда");
+        destinationPares = new HashMap<>();
+
+        /*SELECT tdt.destinationCode, tdt.destinationRussianName FROM TaxiTable tt
+        LEFT OUTER JOIN TaxiDestinationTable tdt
+        ON tdt.destinationCode = tt.destinationCode
+        WHERE tt.fromCode like '0'   */
+
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT tdt.destinationCode, tdt.destinationRussianName FROM ")
+                .append("TaxiTable tt ")
+                .append("LEFT OUTER JOIN TaxiDestinationTable tdt ")
+                .append("ON tdt.destinationCode = tt.destinationCode ")
+                .append("WHERE tt.fromCode like ?");
+
+        helper = new MySQLiteHelper(getContext());
+        Cursor cursor = helper.getReadableDatabase().rawQuery(sb.toString(),new String[] {fromCode});
+
+        if (!cursor.moveToFirst()) return;
+        while (!cursor.isAfterLast()) {
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(TaxiDestinationTable.COLUMN_TAXIDESTINATION_DESTINATIONRUSSIANNAME));
+            String code = cursor.getString(cursor.getColumnIndexOrThrow(TaxiDestinationTable.COLUMN_TAXIDESTINATION_DESTINATIONCODE));
+
+            destinationArray.add(name);
+            destinationPares.put(name, code);
+
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, destinationArray); //selected item will look like a spinner set from XML
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTo.setAdapter(spinnerArrayAdapter);
+
+
+
+    }
 
 
 
@@ -161,7 +204,7 @@ public class TaxiFragment extends Fragment {
         String destinationcode;
 
 
-        MySQLiteHelper helper = new MySQLiteHelper(getActivity());
+        helper = new MySQLiteHelper(getActivity());
 
         cursor = helper.getReadableDatabase().query(
                 TaxiTable.TABLE_TAXI,
@@ -175,95 +218,6 @@ public class TaxiFragment extends Fragment {
         );
         return null;
     }
+
+
 }
-
-
-
-
-
-    /*private void loadSpinnerData() {
-        listTaxiFrom = new ArrayList<String>();
-        helper.getReadableDatabase();
-
-        Cursor taxiFromCursor = helper.getReadableDatabase().query(TaxiTable.COLOMN_TAXI_FROM,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null);
-        taxiFromCursor.moveToFirst();
-        if (!taxiFromCursor.isAfterLast()) {
-            do {
-                String taxiFrom = taxiFromCursor.getString(1);
-                spinnerFrom.addView(taxiFrom);
-            } while (taxiFromCursor.moveToNext());
-        }
-        taxiFromCursor.close();
-        helper.close();   // Закрываем БД
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        addItemsOnSpinner();
-    }
-
-    public void addItemsOnSpinner() {
-
-        ArrayAdapter<CharSequence> adapterFrom = ArrayAdapter.createFromResource(getActivity(), R.array.spinnerfrom, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> adapterTo = ArrayAdapter.createFromResource(getActivity(), R.array.spinnerto, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> adapterCar = ArrayAdapter.createFromResource(getActivity(), R.array.spinnerTypeOfCar, android.R.layout.simple_spinner_item);
-
-        adapterFrom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapterTo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapterCar.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerFrom.setAdapter(adapterFrom);
-        spinnerTo.setAdapter(adapterTo);
-        spinnerCar.setAdapter(adapterCar);
-    }*/
-
-
-
-
-
-
-
-
-
-
- /*String[] listTaxiFrom = new String[] {"_id", "from"};
-        helper = new MySQLiteHelper(getActivity());
-
-        spinnerFromAdapter = new SimpleCursorAdapter(getActivity(),
-                android.R.layout.simple_spinner_item,
-                null,                                                                               //Вместо курсора передаем null, ибо CursorLoader по окончании выборки сам создаст нам курсор.
-                new String[]{"from"},
-                new int[]{android.R.id.text1},
-                0);                                                                                 //индекс CursorLoader-а.
-        spinnerFromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerFrom.setAdapter(spinnerFromAdapter);
-
-        getActivity().getSupportLoaderManager().initLoader(0, null, this);
-public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-    // Готовим колонки и условие
-    String[] from = new String[] { "_id", "from" };
-    // создаем и возвращаем настроенный лоадер поиска
-    return new CursorLoader(getActivity(), URI, from, null, null, null);
-}
-
-    // обработчик окончания выборки из БД
-    @Override
-    public void onLoadFinished(Loader arg0, Cursor arg1) {
-        spinnerFromAdapter.swapCursor(arg1);
-        break;
-    }
-
-
-    // Этот метод нам без надобности
-
-    @Override
-    public void onLoaderReset(Loader arg0) {
-
-    }*//*
-*/
