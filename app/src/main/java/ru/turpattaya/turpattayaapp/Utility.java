1b2cdc9396ebef1c4815c8987878f3e5817d8ab8
Alshin Anton <alshinanton@gmail.com>
2016-12-13 02:30:02 +0700

    Finish Ruble

--- app/src/main/java/ru/turpattaya/turpattayaapp/Utility.java
+++ app/src/main/java/ru/turpattaya/turpattayaapp/Utility.java
@@ -12,25 +12,25 @@ import java.util.regex.Pattern;
 public class Utility {
 
     // выдергиваем курс доллара из HTML кода сгенерированного с помощью Build
-    private static final String regexpRuble ="Russian Ruble\\s*</td><td class=\"D3\">(\\d+\\.\\d+)" +
-            "\\s*</td><td class=\"D4\">\\W</td><td class=\"D4\">\\W</td><td class=\"D4\">\\W" +
-            "</td><td class=\"D4\">\\W</td><td class=\"D5\">(\\d+\\.\\d+)\\s*</td></tr>";
-   /* private static final String regexpUSD1 ="USD : 1\\s*</td><td class=\"D3\">(\\d+\\.\\d+)</td><td class=\"D4\">35.37</td><td class=\"D4\">35.37</td><td class=\"D4\">35.47</td><td class=\"D4\">35.77</td><td class=\"D5\">(\\d+\\.\\d+)";
-    private static final String regexpUSD5_20 ="USD : 5-20</td><td class=\"D3\">(\\d+\\.\\d+)</td><td class=\"D4\">35.37</td><td class=\"D4\">35.37</td><td class=\"D4\">35.47</td><td class=\"D4\">35.77</td><td class=\"D5\">(\\d+\\.\\d+)";
+    private static final String regexpRuble ="Russian Ruble\\s*</td><td class=\"D3\">(\\d+\\.\\d+)\\s*</td><td class=\"D4\">-</td><td class=\"D4\">-</td><td class=\"D4\">-</td><td class=\"D4\">-</td><td class=\"D5\">(\\d+\\.\\d+)";
+    private static final String regexpUSD1 ="USD\\s+:\\s+1\\s*</td><td class=\"D3\">(\\d+\\.\\d+)</td><td class=\"D4\">35.37</td><td class=\"D4\">35.37</td><td class=\"D4\">35.47</td><td class=\"D4\">35.77</td><td class=\"D5\">(\\d+\\.\\d+)";
+    /*private static final String regexpUSD5_20 ="USD : 5-20</td><td class=\"D3\">(\\d+\\.\\d+)</td><td class=\"D4\">35.37</td><td class=\"D4\">35.37</td><td class=\"D4\">35.47</td><td class=\"D4\">35.77</td><td class=\"D5\">(\\d+\\.\\d+)";
     private static final String regexpUSSD50_100 ="USD : 50-100</td><td class=\"D3\">(\\d+\\.\\d+)</td><td class=\"D4\">35.37</td><td class=\"D4\">35.37</td><td class=\"D4\">35.47</td><td class=\"D4\">35.77</td><td class=\"D5\">(\\d+\\.\\d+)";
     private static final String regexpEURO ="Euro</td><td class=\"D3\">(\\d+\\.\\d+)</td><td class=\"D4\">37.33041</td><td class=\"D4\">37.33041</td><td class=\"D4\">37.41625</td><td class=\"D4\">38.1925</td><td class=\"D5\">(\\d+\\.\\d+)";*/
 
 
     private static final Pattern patternRuble = Pattern.compile(regexpRuble);
-/*    private static final Pattern patternUSD1 = Pattern.compile(regexpUSD1);
-    private static final Pattern patternUSD5_20 = Pattern.compile(regexpUSD5_20);
+    private static final Pattern patternUSD1 = Pattern.compile(regexpUSD1);
+/*    private static final Pattern patternUSD5_20 = Pattern.compile(regexpUSD5_20);
     private static final Pattern patternUSD50_100 = Pattern.compile(regexpUSSD50_100);
     private static final Pattern patternEURO = Pattern.compile(regexpEURO);*/
 
     public static void main(String[] args) {
         String kassikorn = getPage("http://www.kasikornbank.com/EN/RatesAndFees/ForeignExchange/Pages/ForeignExchange.aspx");
         System.out.println(getRuble(kassikorn));
+        System.out.println(getUsd1(kassikorn));
     }
+
 
     // метод который будет обрабатывать страницу
     public static String getPage(String pageUrl) {
@@ -63,13 +63,28 @@ public class Utility {
         return buffer.toString();
     }
 
-    public static String getRuble(String text) {
-        String result = "";
+    private static String getRuble(String text) {
+        String resultRubBy = "";
+        String resultRubSell = "";
         Matcher matcherRuble = patternRuble.matcher(text);
 
         if (matcherRuble.find()) {
-            result = matcherRuble.group(1);
+            resultRubBy = matcherRuble.group(1);
+            resultRubSell = matcherRuble.group(2);
         }
-        return result;
+        return "By Rub  " + resultRubBy+ "    Sell Rub  " + resultRubSell;
     }
+
+    private static String getUsd1(String text) {
+        String resultUSD1By = "";
+        String resultUSD1Sell = "";
+        Matcher matcherUSD1 = patternUSD1.matcher(text);
+
+        if (matcherUSD1.find()) {
+            resultUSD1By = matcherUSD1.group(1);
+            resultUSD1Sell = matcherUSD1.group(2);
+        }
+        return "By USD1  " + resultUSD1By+ "    Sell USD1 " + resultUSD1Sell;
+    }
+
 }
