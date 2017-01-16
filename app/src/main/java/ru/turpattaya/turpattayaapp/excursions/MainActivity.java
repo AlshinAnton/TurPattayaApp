3c29863971efccc8310cb1b8be6afec3062a0d9f
Alshin Anton <alshinanton@gmail.com>
2017-01-17 01:11:28 +0700

    try to add Search ((( false

--- app/src/main/java/ru/turpattaya/turpattayaapp/excursions/MainActivity.java
+++ app/src/main/java/ru/turpattaya/turpattayaapp/excursions/MainActivity.java
@@ -1,13 +1,18 @@
 package ru.turpattaya.turpattayaapp.excursions;
 
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+
 import ru.turpattaya.turpattayaapp.BaseActivity;
 import ru.turpattaya.turpattayaapp.MySQLiteHelper;
 import ru.turpattaya.turpattayaapp.R;
@@ -17,6 +22,7 @@ public class MainActivity extends BaseActivity {
     ListView list;
     String orderBy = "";
     static String selection= "";
+    MySQLiteHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,7 @@ public class MainActivity extends BaseActivity {
 
         list = (ListView) findViewById(R.id.list_excursion);
 
-        MySQLiteHelper helper = new MySQLiteHelper(this);
+        helper = new MySQLiteHelper(this);
 
         Cursor cursor = helper.getReadableDatabase().query(
                 ExcursionTable.TABLE_EXCURSION,
@@ -52,6 +58,33 @@ public class MainActivity extends BaseActivity {
             searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
                 @Override
                 public boolean onQueryTextChange(String newText) {
+                                                    /*ArrayList<String> tempList = new ArrayList<>();
+
+                                                    for(String temp : items) {
+                                                        if (temp.toLowerCase().contains(newText.toLowerCase())) {
+                                                            tempList.add(temp);
+                                                        }
+                                                    }
+                                                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, tempList);
+                                                    list.setAdapter(adapter);
+                                                    return true;*/
+
+                    /*helper = new MySQLiteHelper(MainActivity.this);
+
+                    Cursor cursor = helper.getReadableDatabase().query(
+                            ExcursionDetailTable.TABLE_EXCURSIONDETAIL,
+                            null,
+                            ExcursionTable.COLUMN_EXCURSION_PAGETITLE + " =?",
+                            new String[]{String.valueOf(id)},
+                            null,
+                            null,
+                            null,
+                            null
+                    );
+
+                        cursor.close();*/
+
+
 
                     return true;
                 }
