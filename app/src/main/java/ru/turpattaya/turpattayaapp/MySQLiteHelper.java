ccade5899255f863c4f234d8a42395e32094cab0
Alshin Anton <alshinanton@gmail.com>
2017-01-20 20:37:47 +0700

    Before change SQLiteHelper

--- app/src/main/java/ru/turpattaya/turpattayaapp/MySQLiteHelper.java
+++ app/src/main/java/ru/turpattaya/turpattayaapp/MySQLiteHelper.java
@@ -5,20 +5,32 @@ import android.database.sqlite.SQLiteDatabase;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
+import ru.turpattaya.turpattayaapp.excursions.CategoryTable;
+import ru.turpattaya.turpattayaapp.excursions.ExcursionDetailTable;
 import ru.turpattaya.turpattayaapp.excursions.ExcursionTable;
+import ru.turpattaya.turpattayaapp.excursions.ImagesDetailTable;
+import ru.turpattaya.turpattayaapp.taxi.TaxiDestinationTable;
+import ru.turpattaya.turpattayaapp.taxi.TaxiFromTable;
+import ru.turpattaya.turpattayaapp.taxi.TaxiTable;
 
 public class MySQLiteHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "tourptt.sqlite";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        ExcursionTable.onUpgrade(sqLiteDatabase, i, i1);
-        /*ExcursionDetailTable.onUpgrade(sqLiteDatabase, i, i1);*/
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i3) {
+        ExcursionTable.onUpgrade(sqLiteDatabase, i, i3);
+        ExcursionDetailTable.onUpgrade(sqLiteDatabase, i, i3);
+        CategoryTable.onUpgrade(sqLiteDatabase, i, i3);
+        ImagesDetailTable.onUpgrade(sqLiteDatabase, i, i3);
+        TaxiDestinationTable.onUpgrade(sqLiteDatabase, i, i3);
+        TaxiFromTable.onUpgrade(sqLiteDatabase, i, i3);
+        TaxiTable.onUpgrade(sqLiteDatabase, i, i3);
+        FreeTransfersTable.onUpgrade(sqLiteDatabase, i, i3);
     }
 }
